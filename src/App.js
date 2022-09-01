e77ab6978f7ed0a8f48ddad92932619e6048f775
Nitin Singh <nitinsinghm111@gmail.com>
2022-09-01 23:41:19 +0530

    Simple Birthday Reminder

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+function App() {
+  const [people, setpeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthdays Today</h3>
+        <List people={people} />
+        <button
+          onClick={() => {
+            setpeople([]);
+          }}
+        >
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
